b794865c5a0b480cc20f0f409979be47f651f831
Dan Carlier <dan.carlier@gmail.com>
2022-08-17 16:40:52 +1000

    Created initial layout, added vuetify

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, cumque. Vero totam qui voluptates quo omnis
+      nobis explicabo laborum ab dolores maxime quisquam, voluptatibus ullam veniam, eligendi quam, possimus inventore.
+    </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, cumque. Vero totam qui voluptates quo omnis
+      nobis explicabo laborum ab dolores maxime quisquam, voluptatibus ullam veniam, eligendi quam, possimus inventore.
+    </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, cumque. Vero totam qui voluptates quo omnis
+      nobis explicabo laborum ab dolores maxime quisquam, voluptatibus ullam veniam, eligendi quam, possimus inventore.
+    </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, cumque. Vero totam qui voluptates quo omnis
+      nobis explicabo laborum ab dolores maxime quisquam, voluptatibus ullam veniam, eligendi quam, possimus inventore.
+    </p>
   </div>
 </template>
