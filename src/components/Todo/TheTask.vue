ac25866e03176d1a46d897a1429afab24f1f925f
Dan Carlier <dan.carlier@gmail.com>
2022-08-24 15:22:12 +1000

    Replaced task delete button with task menu - with menu items: Edit, Due date and Delete

--- src/components/Todo/TheTask.vue
+++ src/components/Todo/TheTask.vue
@@ -17,44 +17,24 @@
           >{{ task.title }}
           </v-list-item-title>
         </v-list-item-content>
+
         <v-list-item-action>
-          <v-btn
-            @click.stop="dialogs.delete = true"
-            icon
-          >
-            <v-icon
-              color="primary lighten-1">mdi-delete</v-icon
-            >
-          </v-btn>
+          <task-menu :task="task"/>
         </v-list-item-action>
       </template>
     </v-list-item>
 
     <v-divider></v-divider>
-
-    <dialog-delete
-      v-if="dialogs.delete"
-      @close="dialogs.delete = false"
-      :task="task"
-    />
   </div>
 </template>
 
 <script>
-import DialogDelete from '@/components/Todo/Dialogs/DialogDelete.vue';
   export default {
     props: {
       'task': Object
     },
-    data() {
-      return {
-        dialogs: {
-          delete: false
-        }
-      }
-    },
     components: {
-      "dialog-delete": require("@/components/Todo/Dialogs/DialogDelete.vue").default
+      "task-menu": require("@/components/Todo/TaskMenu.vue").default
     }
   }
 </script>
